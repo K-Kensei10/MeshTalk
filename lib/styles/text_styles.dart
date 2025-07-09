673cd030935bcb9aca09ec11639f2adba7603e17
K-Kensei10 <kktmrn10@gmail.com>
2025-07-09 23:41:59 +0900

    複数ファイルの追加 homescreenを追加 AppBerを追加

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle appBarTitle = TextStyle(
+    fontSize: 80,
+    fontWeight: FontWeight.bold,
+    color: Color.fromARGB(255, 255, 0, 0), // 必要に応じて色も指定
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontSize: 50,
+    color: Color.fromRGBO(49, 16, 199, 0.867),
+  );
+}
