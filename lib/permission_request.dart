9b3336fb6c6f8d9c7632d809f8c77105ca9c8bb9
KEITA-Waffle <219523959+KEITA-Waffle@users.noreply.github.com>
2025-08-12 15:51:11 +0900

    [add]url_launcherの追加

--- lib/permission_request.dart
+++ lib/permission_request.dart
@@ -9,35 +9,40 @@ class PermissionRequestMessage extends StatelessWidget {
     final dialog = AlertDialog(
       title: Text('より良い体験のためBluethoothを使います'),
       content: Text('このアプリではユーザーと通信するためにBluetooth機能が必要です'),
-      actions: <Widget>[
-        Row(
+      actions: [
+        Column(
           children: [
-            //キャンセルボタン
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(2)),
+            Row(
+              children: [
+                //キャンセルボタン
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(2)),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    side: BorderSide(
+                      color: Color.fromARGB(255, 219, 219, 219),
+                      width: 2,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('キャンセル'),
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                side: BorderSide(
-                  color: Color.fromARGB(255, 219, 219, 219),
-                  width: 2,
+                //権限を変更するボタン
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStateProperty.all(Colors.white),
+                    backgroundColor: WidgetStateProperty.all(
+                      const Color.fromARGB(255, 68, 230, 248),
+                    ),
+                  ),
+                  child: Text('権限の設定'),
                 ),
-              ),
-              onPressed: () {},
-              child: Text('キャンセル'),
+              ],
             ),
-            //権限を変更するボタン
-            ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                foregroundColor: WidgetStateProperty.all(Colors.white),
-                backgroundColor: WidgetStateProperty.all(
-                  const Color.fromARGB(255, 68, 230, 248),
-                ),
-              ),
-              child: Text('権限の設定'),
-            ),
+            Text(''),
           ],
         ),
       ],
