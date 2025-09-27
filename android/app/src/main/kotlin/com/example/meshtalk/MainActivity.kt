fd9fe07505f8cd2d367e7fbf35e063d0a235f1a2
Keita Waffle <219523959+KEITA-Waffle@users.noreply.github.com>
2025-09-27 22:49:24 +0900

    [add]BLEclassの構築中、変数定義まで

--- android/app/src/main/kotlin/com/example/meshtalk/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/meshtalk/MainActivity.kt
@@ -1,9 +1,42 @@
 package com.example.meshtalk
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.AdvertisingSetCallback;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanSettings
+import android.Manifest;
+import java.security.Policy
+import java.util.*
+
+//BLT class
+class BluetoothLeController(public val activity : Activity) {
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var enable : Boolean = false
+    private var Scanner : BluetoothLeScanner? = null
+    private lateinit var mScanCallback : ScanCallback
+    private val permissions = arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION)
+    private var scanFilter: ScanFilter? = null
+    private var scanFilterList: ArrayList<ScanFilter> = ArrayList()
+
+    fun ScanStart() {
+        if (enable) return;
+    }
+
+
+
+
+}
+
 
 // これでネイティブ側のプログラムを実行してる
 class MainActivity : FlutterActivity() {
@@ -21,13 +54,21 @@ class MainActivity : FlutterActivity() {
                 val messageType = call.argument<String>("messageType")
                 val targetPhoneNum = call.argument<String>("targetPhoneNum")
                 val TTL = 150
+                val separator = "";/:""
 
-                val disaster_message_data = messageType + phoneNum + targetPhoneNum + TTL + message
+                val disaster_message_data = messageType + separator + phoneNum +separator + targetPhoneNum + separator + TTL + separator + message
+            } else {
+                result.notImplemented()
+            }
 
-                result.success("出力されたデータはこれです $disaster_message_data")
+            if (call.method == "sendMessage") {
+              val bluetoothStatus = true
             } else {
                 result.notImplemented()
             }
         }
     }
 }
+
+
+
